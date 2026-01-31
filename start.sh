111a4932edbd0582417647486303218d3d8bbce6
codercollo <itscollinsmaina@gmail.com>
2026-01-31 17:04:56 +0300

    Fix: Use export for environment variables in Alpine sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-
 set -e
+
 echo "run db migration"
-source /app/app.env
+
+# Export environment variables from app.env
+export $(grep -v '^#' /app/app.env | xargs)
+
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
-exec "$@" 
+exec "$@"
